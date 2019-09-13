de3fd98a927e93fa6eb024852a970427fa5fb742
ajay-wohlig <ajay.yadav@wohlig.in>
2019-09-13 11:30:20 +0530

    validator and lodash

--- test_loadash.js
+++
@@ -1,9 +0,0 @@
-console.log("_.isArrayLike");
-
-console.log(
-  _.isArrayLike([1, 2, 3]),               // => true
-  _.isArrayLike(document.body.children),  // => true
-  _.isArrayLike('abc'),                   // => true
-  _.isArrayLike(_.noop),                  // => false
-  _.reverse([1,2,3,4,5])
-);
---
+++ testlodash.js
@@ -0,0 +1,5 @@
+const _ = require("lodash")
+
+console.log("Reverse")
+
+console.log(_.reverse([1,2,3,4,5]))
