6bfca569d9aab7dbd1d545da9be281d04021aac6
ajay-wohlig <ajay.yadav@wohlig.in>
2019-09-18 10:01:09 +0530

    framework

---
+++ node-course/notes-app/app.js
@@ -0,0 +1,15 @@
+const validator = require("validator")
+//const chalk = require("chalk")
+
+//const notes = require('./notes.js')
+
+//const command = process.argv[2]
+
+console.log(validator.isEmail("ajaySpeechGrammarList.com"))
+
+// if(command == "add"){
+//     console.log("Adding note")
+// }
+// else if(command == "remove"){
+//     console.log("Removing note")
+// }
